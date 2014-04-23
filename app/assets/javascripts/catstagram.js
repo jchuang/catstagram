44770404005be224b4e2e668509a5df182ed8ca3
Janet Chuang <janet.chuang@gmail.com>
2014-04-22 22:28:03 -0400

    meow button submits asynchronously

---
+++ app/assets/javascripts/catstagram.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+  $('[data-meow-button="create"]').on('submit', function(event) {
+    event.preventDefault();
+
+    $form = $(event.currentTarget);
+
+    $.ajax({
+      type: "POST",
+      url: $form.attr('action'),
+      dataType: "json",
+      success: function() {
+        alert('meow!');
+      }
+    });
+  });
+});
